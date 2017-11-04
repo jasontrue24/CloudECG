debc79e26012e9624746f7e94749549584b3f746
qz58 <31520213+qidizhai@users.noreply.github.com>
2017-11-03 21:22:46 -0400

    update some Pepe 8 change

--- HeartRateClass.py
+++ HeartRateClass.py
@@ -4,7 +4,7 @@ from flask import Flask, request, jsonify
 class ECG:
 
     def __init__(self, req):
-        self.threshold = 0.16
+        self.threshold = 0.42
         self.bradycardia = 40
         self.tachycardia = 120
         self.checkbrady = []
@@ -15,29 +15,42 @@ class ECG:
         self.averageHR = []
 
     def getInHR(self):
-        from instHR import instHR
-        from instHR import getPeaks
+        from bme590hrm.instHR import instHR
+        from bme590hrm.instHR import getPeaks
+        peaks = getPeaks(self.mV, self.time, self.threshold)
         for eachtime in self.time:
             if eachtime is None:
                 eachtime = 0
-            self.instHR.append(instHR(getPeaks(self.mV, self.time, +
-                                               self.threshold), eachtime))
+            self.instHR.append(instHR(peaks, eachtime))
 
     def getAverage(self, req):
         # try except for the cases when st and et are out of bounds
         avglength = req['averaging_period']
-        from getAverage import getAverage
-        for idx, val in enumerate(self.time):
-            # if val == None
-            if idx == 0 or idx % avglength == 0 and (idx+avglength-1) <
-            len(self.time):
-                st = self.time[idx]
-                et = self.time[idx+avglength-1]
-            self.averageHR.append(getAverage(self.mV, self.time, +
-                                             st, et, self.threshold))
+        from bme590hrm.getAverage import getAverage
+        from bme590hrm.instHR import getPeaks
+        from bme590hrm.instHR import instHR
+        peaks = getPeaks(self.mV, self.time, self.threshold)
+        inst_HR_values = []
+        for eachtime in self.time:
+            if eachtime is None:
+                eachtime = 0
+            inst_HR_values.append(instHR(peaks, eachtime))
+        # print len(inst_HR_values), len(self.time)
+        for i in range(0, len(inst_HR_values)):
+            eachtime = self.time[i]
+            mintime = eachtime - avglength/2.
+            maxtime = eachtime + avglength/2.
+            instants_counter = 0.
+            instants_sum = 0.
+            for j in range(0, len(inst_HR_values)):
+                if self.time[j] >= mintime and self.time[j] <= maxtime:
+                    instants_counter += 1
+                    instants_sum += inst_HR_values[j]
+            print instants_counter, instants_sum
+            self.averageHR.append(instants_sum/instants_counter)
 
     def getcheckbradyandtachy(self, group):
-        from checkbradyandtachycardia import checkbradyandtachycardia
+        from bme590hrm.checkbradyandtachycardia import checkbradyandtachycardia
         tach = self.tachycardia
         brad = self.bradycardia
         for eachval in group:
