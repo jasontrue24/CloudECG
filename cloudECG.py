8540bd75c537f0cdf39a07b2b4683489d21ca722
Jiang Baowei <jasontrue24@gmail.com>
2017-11-03 21:32:19 -0400

    PEP8 check #2

--- cloudECG.py
+++ cloudECG.py
@@ -1,63 +1,93 @@
 # request count for every request, including GET
 # how to test post/get through VM
 # RFC document will serve as the grading rubric
-# google hangout 
+# google hangout
 
 from HeartRateClass import ECG
 from flask import Flask, request, jsonify
 app = Flask(__name__)
 count = 0
 
+
 @app.route("/api/heart_rate/summary", methods=['POST'])
 def summary():
-	global count 
-	count += 1
-	req = request.get_json()
-	ECG1 = ECG(req)
-	ECG1.getInHR()
-	ECG1.getcheckbradyandtachy(ECG1.instHR)
-	data = {"time": ECG1.time,
-                "voltage": ECG1.mV,
+    """
+    Using Json input "time" and "voltage"
+
+    :return: the time list, the voltage list, instantaneous heart rate
+    and the tachycardia and bradycardia result
+    """
+    global count
+    count += 1
+    try:
+        req = request.get_json()
+        ECG1 = ECG(req)
+        for eachvoltage in ECG1.mV:
+            if eachvoltage > 150 or eachvoltage < -100:
+                return jsonify("The voltage is out of bound"), 400
+        ECG1.getInHR()
+        ECG1.getcheckbradyandtachy(ECG1.instHR)
+        data = {"time": ECG1.time, "voltage": ECG1.mV,
                 "instantaneous_heart_rate": ECG1.instHR,
                 "tachycardia_annotations": ECG1.checktachy,
                 "bradycardia_annotations": ECG1.checkbrady}
-	return jsonify(data)
+        return jsonify(data)
+    except:
+        return jsonify("The input time is not in a correct format"), 400
+
 
 @app.route("/api/heart_rate/average", methods=['POST'])
 def average():
-	global count 
-	count += 1
-	req = request.get_json()
-	ECG1 = ECG(req)
-	ECG1.getAverage(req)
-	ECG1.getcheckbradyandtachy(ECG1.averageHR)
-	data = {"averaging_period": req['averaging_period'],
+    """
+    Using Json input "Time" and "voltage"
+
+    :return: the averaging_period, time_interval, average heart rate
+    tachycardia and bradycardia annotation
+    """
+    global count
+    count += 1
+    try:
+        req = request.get_json()
+        ECG1 = ECG(req)
+        for eachvoltage in ECG1.mV:
+            if eachvoltage > 150 or eachvoltage < -100:
+                return jsonify("The voltage is out of bound"), 400
+        ECG1.getAverage(req)
+        ECG1.getcheckbradyandtachy(ECG1.averageHR)
+        data = {"averaging_period": req['averaging_period'],
                 "time_interval": ECG1.time,
                 "average_heart_rate": ECG1.averageHR,
                 "tachycardia_annotations": ECG1.checktachy,
                 "bradycardia_annotations": ECG1.checkbrady}
-	return jsonify(data)
+        return jsonify(data)
+    except:
+        return jsonify("The input time is not in a correct format"), 400
+
 
 @app.route("/api/requests")
 def requests():
-	global count 
-	count += 1
-	data = {"number of requests": count}
-	return jsonify(data)
+	"""
 
-	# try:
-	# 	pass
-	# except Exception as e:
-	# 	raise
-	# else:
-	# 	pass
-	# finally:
-	# 	pass
+    :return: the total number of the requests the web service has served
+    """
+    global count
+    count += 1
+    data = {"number of requests": count}
+    return jsonify(data)
 
-	# 	except ValueError 
-		# flask-specific error handling: decorator
-		# different 'levels' of errors
-		# virtual machine is running the server 
-		# how to display http error: logging, sending the error back to the client
-		# it's good to have try except for specific errors instead of general errors
 
+    # try:
+    # 	pass
+    # except Exception as e:
+    # 	raise
+    # else:
+    # 	pass
+    # finally:
+    # 	pass
+
+    # 	except ValueError
+        # flask-specific error handling: decorator
+        # different 'levels' of errors
+        # virtual machine is running the server
+        # how to display http error: logging, sending the error back to the client
+        # it's good to have try except for specific errors instead of general errors
