a296bda6172a93cf9438b9a5f8a667b275313c13
jasontrue24 <31495842+jasontrue24@users.noreply.github.com>
2017-11-03 22:37:11 -0400

    Update cloudECG.py

--- cloudECG.py
+++ cloudECG.py
@@ -16,24 +16,26 @@ def summary():
     :return: the time list, the voltage list, instantaneous heart rate
     and the tachycardia and bradycardia result
     """
-    global count
-    count += 1
     try:
-        req = request.get_json()
-        ECG1 = ECG(req)
-        for eachvoltage in ECG1.mV:
-            if eachvoltage > 150 or eachvoltage < -100:
-                return jsonify("The voltage is out of bound"), 400
-        ECG1.getInHR()
-        ECG1.getcheckbradyandtachy(ECG1.instHR)
-        data = {"time": ECG1.time, "voltage": ECG1.mV,
+        global count
+        count += 1
+        try:
+            req = request.get_json()
+            ECG1 = ECG(req)
+            for eachvoltage in ECG1.mV:
+                if eachvoltage > 150 or eachvoltage < -100:
+                    return jsonify("The voltage is out of bound"), 400
+            ECG1.getInHR()
+            ECG1.getcheckbradyandtachy(ECG1.instHR)
+            data = {"time": ECG1.time, "voltage": ECG1.mV,
                 "instantaneous_heart_rate": ECG1.instHR,
                 "tachycardia_annotations": ECG1.checktachy,
                 "bradycardia_annotations": ECG1.checkbrady}
-        return jsonify(data)
+            return jsonify(data), 200
+        except:
+            return jsonify("The input time is not in a correct format"), 400
     except:
-        return jsonify("The input time is not in a correct format"), 400
-
+        return jsonify("There is an internal error"), 500
 
 @app.route("/api/heart_rate/average", methods=['POST'])
 def average():
@@ -42,32 +44,37 @@ def average():
     :return: the averaging_period, time_interval, average heart rate
     tachycardia and bradycardia annotation
     """
-    global count
-    count += 1
     try:
-        req = request.get_json()
-        ECG1 = ECG(req)
-        for eachvoltage in ECG1.mV:
-            if eachvoltage > 150 or eachvoltage < -100:
-                return jsonify("The voltage is out of bound"), 400
-        ECG1.getAverage(req)
-        ECG1.getcheckbradyandtachy(ECG1.averageHR)
-        data = {"averaging_period": req['averaging_period'],
+        global count
+        count += 1
+        try:
+            req = request.get_json()
+            ECG1 = ECG(req)
+            for eachvoltage in ECG1.mV:
+                if eachvoltage > 150 or eachvoltage < -100:
+                    return jsonify("The voltage is out of bound"), 400
+            ECG1.getAverage(req)
+            ECG1.getcheckbradyandtachy(ECG1.averageHR)
+            data = {"averaging_period": req['averaging_period'],
                 "time_interval": ECG1.time,
                 "average_heart_rate": ECG1.averageHR,
                 "tachycardia_annotations": ECG1.checktachy,
                 "bradycardia_annotations": ECG1.checkbrady}
-        return jsonify(data)
+            return jsonify(data), 200
+        except:
+            return jsonify("The input time is not in a correct format"), 400
     except:
-        return jsonify("The input time is not in a correct format"), 400
-
+        return jsonify("There is an internal error"), 500
 
 @app.route("/api/requests")
 def requests():
     """
     :return: the total number of the requests the web service has served
     """
-    global count
-    count += 1
-    data = {"number of requests": count}
-    return jsonify(data)
+    try:
+        global count
+        count += 1
+        data = {"number of requests": count}
+        return jsonify(data), 200
+    except:
+        return jsonify("There is an internal error"), 500
