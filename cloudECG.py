f06408e52000ef8f19ca0009bc4d4dada19a4163
qz58 <31520213+qidizhai@users.noreply.github.com>
2017-11-03 23:12:02 -0400

    Update cloudECG.py

--- cloudECG.py
+++ cloudECG.py
@@ -7,13 +7,7 @@ from HeartRateClass import ECG
 from flask import Flask, request, jsonify
 app = Flask(__name__)
 count = 0
-
-def internal_errorchecking():
-    try:
-        @app.route("/api/heart_rate/summary", methods=['POST'])
-    except:
-        return jsonify("There is an internal error"), 500
-
+ 
     
 @app.route("/api/heart_rate/summary", methods=['POST'])
 def summary():
@@ -47,37 +41,31 @@ def average():
     :return: the averaging_period, time_interval, average heart rate
     tachycardia and bradycardia annotation
     """
+    global count
+    count += 1
     try:
-        global count
-        count += 1
-        try:
-            req = request.get_json()
-            ECG1 = ECG(req)
-            for eachvoltage in ECG1.mV:
-                if eachvoltage > 150 or eachvoltage < -100:
-                    return jsonify("The voltage is out of bound"), 400
-            ECG1.getAverage(req)
-            ECG1.getcheckbradyandtachy(ECG1.averageHR)
-            data = {"averaging_period": req['averaging_period'],
+        req = request.get_json()
+        ECG1 = ECG(req)
+        for eachvoltage in ECG1.mV:
+            if eachvoltage > 150 or eachvoltage < -100:
+                return jsonify("The voltage is out of bound"), 400
+        ECG1.getAverage(req)
+        ECG1.getcheckbradyandtachy(ECG1.averageHR)
+        data = {"averaging_period": req['averaging_period'],
                 "time_interval": ECG1.time,
                 "average_heart_rate": ECG1.averageHR,
                 "tachycardia_annotations": ECG1.checktachy,
                 "bradycardia_annotations": ECG1.checkbrady}
-            return jsonify(data), 200
-        except:
-            return jsonify("The input time is not in a correct format"), 400
+        return jsonify(data), 200
     except:
-        return jsonify("There is an internal error"), 500
+        return jsonify("The input time is not in a correct format"), 400
 
 @app.route("/api/requests")
 def requests():
     """
     :return: the total number of the requests the web service has served
     """
-    try:
-        global count
-        count += 1
-        data = {"number of requests": count}
-        return jsonify(data), 200
-    except:
-        return jsonify("There is an internal error"), 500
+    global count
+    count += 1
+    data = {"number of requests": count}
+    return jsonify(data), 200
